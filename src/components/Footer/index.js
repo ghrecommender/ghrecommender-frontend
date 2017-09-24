8babfb8f4c21ffa5283cf19853f0556ca003c8ad
Egor Yurtaev <yurtaev.egor@gmail.com>
2017-09-24 13:27:33 +0600

    Add icons to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Container, Header, Segment, Grid, List } from 'semantic-ui-react'
+import { Container, Header, Segment, Grid, List, Icon } from 'semantic-ui-react'
 import { trackClick } from '../../utils'
 
 const Footer = () => (
@@ -11,13 +11,34 @@ const Footer = () => (
           <Grid.Column width={6}>
             <Header inverted as="h4" content="About" />
             <List link inverted>
-              <List.Item as="a" href="mailto:hire@ghrecommender.io" onClick={trackClick('Footer - Hire us')}>Hire us</List.Item>
-              <List.Item as="a" href="https://github.com/ghrecommender" onClick={trackClick('Footer - Github')}>GitHub</List.Item>
-              <List.Item as="a" href="https://twitter.com/ghrecommender" onClick={trackClick('Footer - Twitter')}>Twitter</List.Item>
+              <List.Item
+                as="a"
+                href="mailto:hire@ghrecommender.io"
+                onClick={trackClick('Footer - Hire us')}
+              >
+                <Icon name="mail outline" /> Hire us
+              </List.Item>
+              <List.Item
+                as="a"
+                href="https://github.com/ghrecommender"
+                onClick={trackClick('Footer - Github')}
+              >
+                <Icon name="github" /> GitHub
+              </List.Item>
+              <List.Item
+                as="a"
+                href="https://twitter.com/ghrecommender"
+                onClick={trackClick('Footer - Twitter')}
+              >
+                <Icon name="twitter" /> Twitter
+              </List.Item>
             </List>
           </Grid.Column>
           <Grid.Column width={6}>
-            <p>Copyright © 2017  <a href="https://github.com/avli">Andrey Lisin</a> and <a href="http://github.com/yurtaev">Egor Yurtaev</a></p>
+            <p>
+              Copyright © 2017 <a href="https://github.com/avli">Andrey Lisin</a> and{' '}
+              <a href="http://github.com/yurtaev">Egor Yurtaev</a>
+            </p>
             <p>The code is licensed under MIT.</p>
           </Grid.Column>
         </Grid.Row>
