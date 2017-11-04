a826f6697a8cfc59b4d6261df557ad95eccc4289
Egor Yurtaev <yurtaev.egor@gmail.com>
2017-11-04 16:37:08 +0600

    Upgrade to React 16

--- src/components/Statistics/index.js
+++ src/components/Statistics/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Statistic } from 'semantic-ui-react'
 
 const items = [
-  { label: 'Users', value: '19,925,204' },
-  { label: 'Repositories', value: '64,245,061' },
-  { label: 'Stars', value: '81,302,007' },
+  { label: 'Users', value: '19,925,204', key: 'users' },
+  { label: 'Repositories', value: '64,245,061', key: 'repositories' },
+  { label: 'Stars', value: '81,302,007', key: 'stars' },
 ]
 
 const StatisticGroups = () => (
